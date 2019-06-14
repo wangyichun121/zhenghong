4663c1093ec9b01a8c4ad21667a5145b02687840
Yihchun Wang <wangyichun882539@163.com>
2019-06-14 14:48:11 +0800

    修改了首页

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,13 +9,13 @@
         <title>Laravel</title>
  
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="{{mix('css/app.css')}}" rel="stylesheet" >
     </head>
     <body>
     <div id="app">
             <!-- 加入vue组件 -->
             <example-component></example-component>
         </div>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{mix('js/app.js') }}"></script>
     </body>
 </html>
